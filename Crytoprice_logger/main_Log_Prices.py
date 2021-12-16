b9910c5d32bd7493ac2b6ea9c6142c8dd4bd925f
GameTL <limsila.limsila@yahoo.com>
2021-12-17 02:14:44 +0700

    Logging Crypto V3
    using an API instead of using requesting an entire page.
    More efficient code.
    Time in seconds for easier conversion
    no commas for the numbers.

--- Crytoprice_logger/main_Log_Prices.py
+++ Crytoprice_logger/main_Log_Prices.py
@@ -3,53 +3,46 @@
 # based on this https://www.scrapingbee.com/blog/selenium-python/
 # interpreter Anaconda 3.8.5 with clearterminal
 # XPATH == path for fetched html
-
-import requests
-from bs4 import BeautifulSoup as bs
-import datetime
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-import clearterminal
+# https://tcoil.info/how-to-get-price-data-for-bitcoin-and-cryptocurrencies-with-python-json-restful-api/
+#%%
 import time
+import requests
+from datetime import datetime
+# pretty printing of pandas dataframe
 
-def get_btc_usd():
-    page = requests.get('https://finance.yahoo.com/quote/BTC-USD/').text
-    return bs(page, "html.parser").find("span", class_ = 'Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)').contents[0]
-
-def get_eth_usd():
-    page = requests.get('https://finance.yahoo.com/quote/ETH-USD/').text
-    return bs(page, "html.parser").find("span", class_ = 'Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)').contents[0]
-
-def get_eth_btc():
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")
-    driver = webdriver.Chrome(options=chrome_options)
-    driver.get('https://finance.yahoo.com/quote/ETH-BTC/')
-    page = driver.page_source
-    driver.quit()
-    return bs(page, "html.parser").find("span", class_ = 'Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)').contents[0]
+def get_current_data(from_sym, to_sym):
+    url = 'https://min-api.cryptocompare.com/data/price'    
+    
+    parameters = {'fsym': from_sym, 'tsyms': to_sym }
+    # response comes as json
+    response = requests.get(url, params=parameters)   
+    #https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD Example of what the link looks like with the parameterss
+    data = response.json()
+    
+    return data[str(list(data)[0])]
 
 def get_time():
-    return datetime.datetime.now()
+    return time.time()
 
+btc_usd = get_current_data('BTC','USD')
+eth_usd = get_current_data('ETH','USD')
+eth_btc = get_current_data('ETH','BTC')
+btc_thb = get_current_data('BTC','THB')
+eth_thb = get_current_data('ETH','THB')
 
 if __name__ == '__main__' :
     """ import timeit
     print('1. :\t' , timeit.timeit(get_btc,number =1), 'milliseconds')
     print('2. :\t' , timeit.timeit(get_eth,number =1), 'milliseconds') """
-
+    
     with open("BitCoinPriceLogging.csv", "a") as file:
-        enitites = [get_time(), get_btc_usd(), get_eth_usd(), get_eth_btc()]
+        enitites = [get_time(), btc_usd, eth_usd, eth_btc, btc_thb, eth_thb]
         for enity in enitites:
             file.write('"' + str(enity) + '",')
         file.write("\n")
-    clearterminal.clear()
-    print('''Cryto price logged: {}
+    print("""Cryto price logged: {}
 
 
 DONE
-DONE'''.format(enitites))
-    time.sleep(3)
-
-
-
+DONE""".format(enitites))
+    time.sleep(3) 
